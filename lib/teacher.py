629ba6af6bce2bf2d93e3496f7f204f511dd2142
Nicholas Muriithi <nickstech707@gmail.com>
2024-05-31 13:12:07 +0300

    Defines classes that inherit from a shared parent, or superclas, methods unique to those classes and  methods in the child class that use the super keyword to inherit from and augment methods in the parent class

--- lib/teacher.py
+++ lib/teacher.py
@@ -7,4 +7,12 @@ import random
 class Teacher(User):
 
     def teach(self):
-        pass
+        return self.knowledge[random.randint(0, len(self.knowledge) - 1)]
+
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Knowledge", "is", "power"]
+
+    
+    
+    
